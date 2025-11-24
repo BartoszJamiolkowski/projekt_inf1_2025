76c2bed95e5c6309424e9e932cfda725bf1e9caf
BartoszJamiolkowski <bjamiolkowski05@gmail.com>
2025-11-24 10:31:14 +0100

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,13 +1,12 @@
-#include "Game.h"
+#include "Game.hpp"
 #include <iostream>
 
 Game::Game()
-    : m_window(sf::VideoMode((unsigned)WIDTH, (unsigned)HEIGHT), "Arkanoid test"),
+    : 
       m_paletka(320.f, 440.f, 100.f, 20.f, 8.f),
       m_pilka(320.f, 250.f, 4.f, 3.f, 8.f)
 {
-    m_window.setFramerateLimit(60);
-    loadLevel();  // tworzenie bloków
+    loadLevel();
 }
 
 void Game::loadLevel()
@@ -19,11 +18,14 @@ void Game::loadLevel()
     const float ROZMIAR_BLOKU_Y = 25.f;
     const float PRZERWA = 2.f;
     const float OFFSET_Y = 50.f;
+
     const float ROZMIAR_BLOKU_X =
         (WIDTH - (ILOSC_KOLUMN - 1) * PRZERWA) / ILOSC_KOLUMN;
 
-    for (int y = 0; y < ILOSC_WIERSZY; ++y) {
-        for (int x = 0; x < ILOSC_KOLUMN; ++x) {
+    for (int y = 0; y < ILOSC_WIERSZY; ++y)
+    {
+        for (int x = 0; x < ILOSC_KOLUMN; ++x)
+        {
             float posX = x * (ROZMIAR_BLOKU_X + PRZERWA);
             float posY = OFFSET_Y + y * (ROZMIAR_BLOKU_Y + PRZERWA);
             int L = (y < 1) ? 3 : (y < 3) ? 2 : 1;
@@ -37,44 +39,20 @@ void Game::loadLevel()
     }
 }
 
-void Game::run()
-{
-    while (m_window.isOpen()) {
-        processEvents();
-        update(m_deltaClock.restart());
-        render();
-    }
-}
-
-void Game::processEvents()
-{
-    sf::Event event;
-    while (m_window.pollEvent(event)) {
-        if (event.type == sf::Event::Closed)
-            m_window.close();
-    }
-
-    // sterowanie
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) ||
-        sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        m_paletka.moveLeft();
-
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) ||
-        sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        m_paletka.moveRight();
-
-    m_paletka.clampToBounds(WIDTH);
-}
-
 void Game::update(sf::Time)
 {
+    // Ruch piłki
     m_pilka.move();
     m_pilka.collideWalls(WIDTH, HEIGHT);
+
+    // Kolizja paletki
     m_pilka.collidePaddle(m_paletka);
 
-    // kolizje piłki z blokami
-    for (auto& blok : m_bloki) {
-        if (!blok.isDestroyed()) {
+    // Kolizje z blokami
+    for (auto& blok : m_bloki)
+    {
+        if (!blok.isDestroyed())
+        {
             sf::FloatRect blokRect = blok.getGlobalBounds();
             sf::FloatRect pilkaRect(
                 m_pilka.getX() - m_pilka.getRadius(),
@@ -83,28 +61,29 @@ void Game::update(sf::Time)
                 m_pilka.getRadius() * 2.f
             );
 
-            if (pilkaRect.intersects(blokRect)) {
+            if (pilkaRect.intersects(blokRect))
+            {
                 blok.trafienie();
                 m_pilka.bounceY();
             }
         }
     }
 
-    if (m_pilka.getY() - m_pilka.getRadius() > HEIGHT) {
+    // Jeżeli piłka spadnie pod ekran
+    if (m_pilka.getY() - m_pilka.getRadius() > HEIGHT)
+    {
         std::cout << "MISS! KONIEC GRY\n";
-        m_window.close();
+        // W tym miejscu możesz zaimplementować reset lub powrót do menu
     }
 }
 
-void Game::render()
+void Game::render(sf::RenderTarget& target)
 {
-    m_window.clear(sf::Color(20, 20, 30));
+    // Rysowanie obiektów
+    m_paletka.draw(target);
+    m_pilka.draw(target);
 
-    m_paletka.draw(m_window);
-    m_pilka.draw(m_window);
-
-    for (auto& b : m_bloki)
-        b.draw(m_window);
-
-    m_window.display();
+    for (auto& blok : m_bloki)
+        blok.draw(target);
 }
+
