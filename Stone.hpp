36d886639afddf85bb35f2e5aabcf07fa29ba779
BartoszJamiolkowski <bjamiolkowski05@gmail.com>
2025-11-27 02:01:27 +0100

    Refactor Stone class: move method definitions to header

--- Stone.hpp
+++ Stone.hpp
@@ -6,49 +6,14 @@ class Stone : public sf::RectangleShape {
 private:
     int m_punktyZycia;
     bool m_jestZniszczony;
-    static const std::array<sf::Color, 4> m_colorLUT;
+    static const std::array<sf::Color, 4> m_colorLUT; // TYLKO deklaracja
 
 public:
-    // Konstruktor
-    Stone(sf::Vector2f startPos, sf::Vector2f rozmiar, int L)
-        : m_punktyZycia(L), m_jestZniszczony(false)
-    {
-        this->setPosition(startPos);
-        this->setSize(rozmiar);
-        this->setOutlineThickness(2.f);
-        aktualizujKolor();
-    }
+    Stone(sf::Vector2f startPos, sf::Vector2f rozmiar, int L);
 
-    // Trafienie blokiem
-    void trafienie() {
-        if (m_jestZniszczony) return;
-        m_punktyZycia--;
-        aktualizujKolor();
-        if (m_punktyZycia <= 0) m_jestZniszczony = true;
-    }
-
-    // Aktualizacja koloru na podstawie punktów życia
-    void aktualizujKolor() {
-        if (m_punktyZycia >= 0 && m_punktyZycia < (int)m_colorLUT.size()) {
-            this->setFillColor(m_colorLUT[m_punktyZycia]);
-        }
-    }
-
-    bool isDestroyed() const {
-        return m_jestZniszczony;
-    }
-
-    void draw(sf::RenderTarget& target) const {
-        if (!m_jestZniszczony) {
-            target.draw(*this);
-        }
-    }
+    void trafienie();
+    void aktualizujKolor();
+    bool isDestroyed() const;
+    void draw(sf::RenderTarget& target) const;
 };
 
-// Definicja statycznej tablicy kolorów
-const std::array<sf::Color, 4> Stone::m_colorLUT = {
-    sf::Color::Transparent, // L = 0
-    sf::Color::Red,         // L = 1
-    sf::Color::Yellow,      // L = 2
-    sf::Color::Blue         // L = 3
-};
