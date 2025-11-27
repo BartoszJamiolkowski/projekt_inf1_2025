a8392bbe8ca23419b267ef07f265a889837f0d23
BartoszJamiolkowski <bjamiolkowski05@gmail.com>
2025-11-27 02:04:23 +0100

    Implement Stone class with health and color logic

---
+++ Stone.cpp
@@ -0,0 +1,41 @@
+#include "Stone.hpp"
+
+const std::array<sf::Color, 4> Stone::m_colorLUT = {
+    sf::Color::Transparent,
+    sf::Color::Red,
+    sf::Color::Yellow,
+    sf::Color::Blue
+};
+
+// Implementacja metod
+Stone::Stone(sf::Vector2f startPos, sf::Vector2f rozmiar, int L)
+    : m_punktyZycia(L), m_jestZniszczony(false)
+{
+    this->setPosition(startPos);
+    this->setSize(rozmiar);
+    this->setOutlineThickness(2.f);
+    aktualizujKolor();
+}
+
+void Stone::trafienie() {
+    if (m_jestZniszczony) return;
+    m_punktyZycia--;
+    aktualizujKolor();
+    if (m_punktyZycia <= 0) m_jestZniszczony = true;
+}
+
+void Stone::aktualizujKolor() {
+    if (m_punktyZycia >= 0 && m_punktyZycia < (int)m_colorLUT.size()) {
+        this->setFillColor(m_colorLUT[m_punktyZycia]);
+    }
+}
+
+bool Stone::isDestroyed() const {
+    return m_jestZniszczony;
+}
+
+void Stone::draw(sf::RenderTarget& target) const {
+    if (!m_jestZniszczony) {
+        target.draw(*this);
+    }
+}
