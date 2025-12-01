a14a9de9e1ef7396fdce2baaea4eec06585d78a1
BartoszJamiolkowski <bjamiolkowski05@gmail.com>
2025-12-01 03:19:47 +0100

    Refactor Paletka class with new position methods
    Added methods for setting position and improved draw method.

--- Paletka.hpp
+++ Paletka.hpp
@@ -3,47 +3,57 @@
 
 class Paletka {
 private:
-	float x, y;// wspolrzedne srodka x,y
-	float szerokosc, wysokosc;//rozmiar paletki
-	float predkosc;//predkosc paletki
-	sf::RectangleShape shape;// rysowanie paletki
+    float x, y;
+    float szerokosc, wysokosc;
+    float predkosc;
+    sf::RectangleShape shape;
+
 public:
-	//parametry startowe paletki 
-	Paletka(float x_, float y_, float s, float w, float p)
-		: x(x_), y(y_), szerokosc(s), wysokosc(w), predkosc(p)
-	{
-		shape.setSize({ szerokosc, wysokosc });
-		shape.setOrigin(szerokosc / 2.f, wysokosc / 2.f);
-		shape.setPosition(x, y);
-		shape.setFillColor(sf::Color::White);
-	}
-	
-	//ruch paletki w lewo
-	void moveLeft() {
-		x = x-predkosc;
-		shape.setPosition(x, y);
-	}
+    Paletka(float x_, float y_, float s, float w, float p)
+        : x(x_), y(y_), szerokosc(s), wysokosc(w), predkosc(p)
+    {
+        shape.setSize({ szerokosc, wysokosc });
+        shape.setOrigin(szerokosc / 2.f, wysokosc / 2.f);
+        shape.setPosition(x, y);
+        shape.setFillColor(sf::Color::White);
+    }
 
-	//ruch paletki w prawo
-	void moveRight() {
-		x = x+predkosc;
-		shape.setPosition(x, y);
-	}
+    void moveLeft() {
+        x -= predkosc;
+        shape.setPosition(x, y);
+    }
 
-	//ograniczenie paletki w grancicach okna 
-	void clampToBounds(float width) {
-		float polowa= szerokosc / 2.f;
-		if (x - polowa < 0.f) x = polowa;
-		if (x + polowa > width) x = width - polowa;
-		shape.setPosition(x, y);
-	}
+    void moveRight() {
+        x += predkosc;
+        shape.setPosition(x, y);
+    }
 
-	void draw(sf::RenderTarget& target) {
-		target.draw(shape);
-	}
-	// gettery do kolizji z pilka
-	float getX() const { return x; }
-	float getY() const { return y; }
-	float getSzerokosc() const { return szerokosc; }
-	float getWysokosc() const { return wysokosc; }
+    void clampToBounds(float width) {
+        float polowa = szerokosc / 2.f;
+        if (x - polowa < 0.f) x = polowa;
+        if (x + polowa > width) x = width - polowa;
+        shape.setPosition(x, y);
+    }
+
+    void setPosition(float nx, float ny) {
+        x = nx;
+        y = ny;
+        shape.setPosition(x, y);
+    }
+
+    void setPosition(const sf::Vector2f& pos) {  // DODANE dla GameState
+        x = pos.x;
+        y = pos.y;
+        shape.setPosition(x, y);
+    }
+
+    void draw(sf::RenderTarget& target) const {
+        target.draw(shape);
+    }
+
+    float getX() const { return x; }
+    float getY() const { return y; }
+    float getSzerokosc() const { return szerokosc; }
+    float getWysokosc() const { return wysokosc; }
 };
+
