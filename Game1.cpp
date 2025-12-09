106389e6bf59fe4c41816ec1ec8c09a59a125967
BartoszJamiolkowski <bjamiolkowski05@gmail.com>
2025-12-09 19:56:44 +0100

    Refactor Game class methods for clarity and efficiency

--- Game1.cpp
+++ Game1.cpp
@@ -4,10 +4,8 @@
 Game::Game(float windowWidth, float windowHeight)
     : m_paletka(windowWidth / 2.f, windowHeight - 60.f, 100.f, 20.f, 8.f),
     m_pilka(windowWidth / 2.f, windowHeight / 2.f, 4.f, 3.f, 8.f),
-    m_windowWidth(windowWidth),
-    m_windowHeight(windowHeight)
-{
-    // Tworzenie bloków
+    m_windowWidth(windowWidth), m_windowHeight(windowHeight) {
+
     const int ILOSC_KOLUMN = 6;
     const int ILOSC_WIERSZY = 7;
     const float ROZMIAR_BLOKU_Y = 25.f;
@@ -19,31 +17,23 @@ Game::Game(float windowWidth, float windowHeight)
         for (int x = 0; x < ILOSC_KOLUMN; ++x) {
             float posX = x * (ROZMIAR_BLOKU_X + PRZERWA);
             float posY = OFFSET_Y + y * (ROZMIAR_BLOKU_Y + PRZERWA);
-            int L = (y < 1) ? 3 : (y < 3) ? 2 : 1;  // Życia bloku zależnie od wiersza
-            m_bloki.emplace_back(sf::Vector2f(posX, posY),
-                sf::Vector2f(ROZMIAR_BLOKU_X, ROZMIAR_BLOKU_Y),
-                L);
+            int L = (y < 1) ? 3 : (y < 3) ? 2 : 1;
+            m_bloki.emplace_back(sf::Vector2f(posX, posY), sf::Vector2f(ROZMIAR_BLOKU_X, ROZMIAR_BLOKU_Y), L);
         }
     }
 }
 
-void Game::update(sf::Time dt)
-{
-    // Sterowanie paletką
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+void Game::update(sf::Time dt) {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
         m_paletka.moveLeft();
-    }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
         m_paletka.moveRight();
-    }
     m_paletka.clampToBounds(m_windowWidth);
 
-    // Aktualizacja piłki
     m_pilka.move();
     m_pilka.collideWalls(m_windowWidth, m_windowHeight);
     m_pilka.collidePaddle(m_paletka);
 
-    // Kolizja piłki z blokami
     for (auto& blok : m_bloki) {
         if (!blok.isDestroyed()) {
             sf::FloatRect blokRect = blok.getGlobalBounds();
@@ -52,33 +42,50 @@ void Game::update(sf::Time dt)
                 m_pilka.getRadius() * 2.f,
                 m_pilka.getRadius() * 2.f);
             if (pilkaRect.intersects(blokRect)) {
+                int hpBefore = blok.getHP();
                 blok.trafienie();
                 m_pilka.bounceY();
+
+                // punkty za kolor bloku
+                switch (hpBefore) {
+                case 3: addScore(15); break;   // czerwony
+                case 2: addScore(10); break;  // żółty
+                case 1: addScore(5); break;  // niebieski
+                }
             }
         }
     }
 
-    // Sprawdzanie przegranej
     if (m_pilka.getY() - m_pilka.getRadius() > m_windowHeight) {
         std::cout << "MISS! KONIEC GRY\n";
         m_gameOver = true;
     }
 }
 
-void Game::render(sf::RenderTarget& target) const
-{
+void Game::render(sf::RenderTarget& target) const {
     m_paletka.draw(target);
     m_pilka.draw(target);
-    for (const auto& blok : m_bloki) {
-        blok.draw(target);
+    for (const auto& blok : m_bloki) blok.draw(target);
+
+    // Wyświetlanie punktów
+    static sf::Font font;
+    static bool fontLoaded = false;
+    if (!fontLoaded) {
+        font.loadFromFile("E:/Windows/Fonts/arial.ttf");
+        fontLoaded = true;
     }
+    sf::Text scoreText;
+    scoreText.setFont(font);
+    scoreText.setString("Punkty: " + std::to_string(m_score));
+    scoreText.setCharacterSize(24);
+    scoreText.setFillColor(sf::Color::White);
+    scoreText.setPosition(target.getSize().x - 150.f, 10.f);
+    target.draw(scoreText);
 }
 
-void Game::reset()
-{
+void Game::reset() {
     m_paletka = Paletka(m_windowWidth / 2.f, m_windowHeight - 60.f, 100.f, 20.f, 8.f);
     m_pilka = Pilka(m_windowWidth / 2.f, m_windowHeight / 2.f, 4.f, 3.f, 8.f);
-
     m_bloki.clear();
 
     const int ILOSC_KOLUMN = 6;
@@ -93,11 +100,9 @@ void Game::reset()
             float posX = x * (ROZMIAR_BLOKU_X + PRZERWA);
             float posY = OFFSET_Y + y * (ROZMIAR_BLOKU_Y + PRZERWA);
             int L = (y < 1) ? 3 : (y < 3) ? 2 : 1;
-            m_bloki.emplace_back(sf::Vector2f(posX, posY),
-                sf::Vector2f(ROZMIAR_BLOKU_X, ROZMIAR_BLOKU_Y),
-                L);
+            m_bloki.emplace_back(sf::Vector2f(posX, posY), sf::Vector2f(ROZMIAR_BLOKU_X, ROZMIAR_BLOKU_Y), L);
         }
     }
-
+    m_score = 0;
     m_gameOver = false;
 }
