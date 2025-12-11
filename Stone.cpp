968d219423535c3602e9d29b25fe9deae8be3dc3
BartoszJamiolkowski <bjamiolkowski05@gmail.com>
2025-12-11 01:18:40 +0100

    Add comments to Stone class methods
    Added comments to methods for better understanding.

--- Stone.cpp
+++ Stone.cpp
@@ -7,7 +7,7 @@ const std::array<sf::Color, 4> Stone::m_colorLUT = {
     sf::Color::Blue
 };
 
-// Implementacja metod
+
 Stone::Stone(sf::Vector2f startPos, sf::Vector2f rozmiar, int L)
     : m_punktyZycia(L), m_jestZniszczony(false)
 {
@@ -17,6 +17,7 @@ Stone::Stone(sf::Vector2f startPos, sf::Vector2f rozmiar, int L)
     aktualizujKolor();
 }
 
+//zmniejszenie HP po trafieniu piłką
 void Stone::trafienie() {
     if (m_jestZniszczony) return;
     m_punktyZycia--;
@@ -24,19 +25,21 @@ void Stone::trafienie() {
     if (m_punktyZycia <= 0) m_jestZniszczony = true;
 }
 
+//aktualizacja koloru bloku według aktualnych punktów życia
 void Stone::aktualizujKolor() {
     if (m_punktyZycia >= 0 && m_punktyZycia < (int)m_colorLUT.size()) {
         this->setFillColor(m_colorLUT[m_punktyZycia]);
     }
 }
 
+//sprawdzenie, czy blok został zniszczony
 bool Stone::isDestroyed() const {
     return m_jestZniszczony;
 }
 
+//rysowanie bloku tylko jeśli nie został zniszczony
 void Stone::draw(sf::RenderTarget& target) const {
     if (!m_jestZniszczony) {
         target.draw(*this);
     }
 }
-
