08e4419d729bf66f6972df60e4ae23b49f9f19f7
BartoszJamiolkowski <bjamiolkowski05@gmail.com>
2025-12-11 01:19:14 +0100

    Refactor comments and formatting in Stone.hpp
    Updated comments and formatting in Stone.hpp

--- Stone.hpp
+++ Stone.hpp
@@ -1,4 +1,4 @@
-#pragma once
+##pragma once
 #include <SFML/Graphics.hpp>
 #include <array>
 
@@ -6,15 +6,17 @@ class Stone : public sf::RectangleShape {
 private:
     int m_punktyZycia;
     bool m_jestZniszczony;
-    static const std::array<sf::Color, 4> m_colorLUT; // tylko deklaracja
+    static const std::array<sf::Color, 4> m_colorLUT; // kolory blokow wg HP
 
 public:
     Stone(sf::Vector2f startPos, sf::Vector2f rozmiar, int L);
 
-    void trafienie();
-    void aktualizujKolor();
+    void trafienie();// zmniejszenie HP o 1
+    void aktualizujKolor();//zmiania koloru
     bool isDestroyed() const;
     void draw(sf::RenderTarget& target) const;
 
-    int getHP() const { return m_punktyZycia; }
+    int getHP() const { 
+        return m_punktyZycia; 
+    }
 };
