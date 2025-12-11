3c95c649b6a7c3af56271f9907b14c50cd3aa0aa
BartoszJamiolkowski <bjamiolkowski05@gmail.com>
2025-12-11 01:10:10 +0100

    Add comments for clarity in Game class
    Added comments to clarify member variables and methods.

--- Game.hpp
+++ Game.hpp
@@ -12,16 +12,22 @@ private:
     std::vector<Stone> m_bloki;
     float m_windowWidth;
     float m_windowHeight;
-    bool m_gameOver = false;
+    bool m_gameOver = false;// flaga informujaca o koncu gry
 
-    int m_score = 0; // aktualny wynik
+    int m_score = 0; // aktualny wynik gracza
 
 public:
     Game(float windowWidth = 800.f, float windowHeight = 600.f);
-    void update(sf::Time dt);
+    void update(sf::Time dt);// aktualizcja stanu gry
     void render(sf::RenderTarget& target) const;
-    bool isGameOver() const { return m_gameOver; }
-    void reset(); // reset stanu gry
-    int getScore() const { return m_score; }
-    void addScore(int pts) { m_score += pts; }
+    bool isGameOver() const { 
+        return m_gameOver; 
+    }
+    void reset(); // reset stanu gry po przegranej 
+    int getScore() const { 
+        return m_score; 
+    }
+    void addScore(int pts) { 
+        m_score += pts; // dodawnie punktow do wyniku
+    }
 };
